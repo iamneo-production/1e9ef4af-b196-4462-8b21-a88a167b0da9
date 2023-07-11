648c973023e86b4284e023e3301eccea0c46ffe4
bavin1522 <pavitraabavin@gmail.com>
2023-07-11 19:55:24 +0000

    code changes

--- .github/workflows/bank_transaction.sql
+++ .github/workflows/bank_transaction.sql
@@ -1,1 +1,14 @@
-select * from bank_transactions;
+select * 
+from BANK_TRANSACTION;
+
+
+SELECT distinct(year), withdrawal_amt
+FROM (
+  SELECT EXTRACT(YEAR FROM "DATE") AS year, 
+         TO_NUMBER(REGEXP_REPLACE(WITHDRAWAL_AMT, '[^0-9.]', '')) AS withdrawal_amt,
+         DENSE_RANK() OVER (PARTITION BY EXTRACT(YEAR FROM "DATE") 
+                            ORDER BY TO_NUMBER(REGEXP_REPLACE(WITHDRAWAL_AMT, '[^0-9.]', '')) DESC) AS rn
+  FROM bank_transaction
+) subquery
+WHERE rn = 4
+order by year;
