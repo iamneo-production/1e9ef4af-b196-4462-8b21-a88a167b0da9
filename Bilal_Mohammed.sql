cf4fe6cbf0ccda30bc512559628c5712a957b851
Bilal1118-collab <billalbillu1118@gmail.com>
2023-07-30 10:18:20 +0000

    final codes

--- Bilal_Mohammed.sql
+++ Bilal_Mohammed.sql
@@ -1,7 +1,6 @@
--- WORKSPACE QUERIES - BANK_TRANSACTION QUERIES --
+-- WORKSPACE QUERIES - BANK_TRANSACTION QUERIES 
 
-/* (1). Query to find Highest Amount debited from the bank
-         in each year*/
+--(1). Query to find Highest Amount debited from the bank in each year
 
    SELECT EXTRACT(YEAR FROM "DATE") AS YEAR, MAX(TO_NUMBER(TRIM(' ' FROM (REPLACE(WITHDRAWAL_AMT, '"', '')))))
    AS HIGHEST_DEBITED_AMOUNT FROM BANK_TRANSACTION 
@@ -10,8 +9,7 @@
    GROUP BY EXTRACT(YEAR FROM "DATE") 
    ORDER BY EXTRACT(YEAR FROM "DATE");
 
-/*(2). Query to find Lowest amount debited from the bank 
-        in each year*/
+--(2). Query to find Lowest amount debited from the bank in each year
 
    SELECT EXTRACT(YEAR FROM "DATE") AS YEAR, MIN(TO_NUMBER(TRIM(' ' FROM (REPLACE(WITHDRAWAL_AMT, '"', ''))))) 
    AS LOWEST_DEBITED_AMOUNT FROM BANK_TRANSACTION 
@@ -20,8 +18,7 @@
    GROUP BY EXTRACT(YEAR FROM "DATE") 
    ORDER BY EXTRACT(YEAR FROM "DATE"); 
 
-/*(3). Query to find the 5th Highest Withdrawal amount 
-        at each year*/
+--(3). Query to find the 5th Highest Withdrawal amount at each year
 
    WITH HIGH_TRANSACTIONS AS(
       SELECT TO_NUMBER(TRIM(' ' FROM REPLACE(WITHDRAWAL_AMT, '"',''))) AS WITHDRAWAL_AMT, 
@@ -36,16 +33,14 @@
    WHERE FHT=5 ORDER BY YEAR;
 
 
-/*(4). Query to count the withdrawal transactions 
-        between May 5 2018, and March 7 2019*/
+--(4). Query to count the withdrawal transactions between May 5 2018, and March 7 2019
 
    SELECT COUNT(WITHDRAWAL_AMT) FROM BANK_TRANSACTION 
    WHERE "DATE" BETWEEN '05-MAY-18' AND '07-MAR-19'
    AND WITHDRAWAL_AMT IS NOT NULL; 
 
 
-/*(5). Query to Find the First five largest Withdrawal transactions are occured 
-         in year 18*/
+--(5). Query to Find the First five largest Withdrawal transactions are occured in year 18
    SELECT TO_NUMBER(TRIM(' ' FROM (REPLACE(WITHDRAWAL_AMT, '"', '')))) 
    AS FIRST_FIVE_LARGEST_TRANSACTIONS_IN_2018 FROM BANK_TRANSACTION 
    WHERE WITHDRAWAL_AMT IS NOT NULL AND EXTRACT(YEAR FROM "DATE")=2018
